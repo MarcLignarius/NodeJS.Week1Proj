cef5bb05fdc31eafd36e0861f0ef4f4d0cbb0cf3
Marc Davies <marcdaviesriot@gmail.com>
2019-05-31 11:39:26 -0700

    add functioning src directory including index.html, main.js. calculator.js and styles.css

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
-import { pingPong } from './ping-pong';
+import { calculator } from './calculator';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  $('#calculator-form').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    var birthdate = $('#birthdate').val();
+    var output = calculator(birthdate);
+    $('#result').text(output);
   });
 });
