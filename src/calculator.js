1b5253229ed686be6ee3a1dc63bc6b3ad71a2d2b
Marc Davies <marcdaviesriot@gmail.com>
2019-05-31 15:13:29 -0700

    test logic for age calculation

--- src/calculator.js
+++ src/calculator.js
@@ -1,15 +1,29 @@
-// export function calculator(birthdate) {
-//   var output = [];
-//   for (var i = 1; i <= goal; i++) {
-//     if (i % 15 === 0) {
-//       output.push("ping-pong");
-//     } else if (i % 3 === 0) {
-//       output.push("ping");
-//     } else if (i % 5 === 0) {
-//       output.push("pong");
-//     } else  {
-//       output.push(i);
-//     }
-//   }
-//   return output;
+// export function calculator() {
+//   let today = new Date();
+//   return today;
 // }
+
+export function calculator() {
+  const mercury = 0.24;
+  const venus = 0.62;
+  const earth = 1;
+  const mars = 1.88;
+  const jupiter = 11.86;
+
+  let ageString = moment("19831107", "YYYYMMDD").fromNow().toString();
+  console.log("ageString", ageString)
+  let age = parseInt(ageString.slice(0, ageString.length -10));
+  console.log("age", age)
+
+
+  let ageOnMercury = Math.floor(age / mercury);
+  console.log("ageOnMercury", ageOnMercury);
+  let ageOnVenus = Math.floor(age / venus);
+  console.log("ageOnVenus", ageOnVenus);
+  let ageOnEarth = Math.floor(age / earth);
+  console.log("ageOnEarth", ageOnEarth);
+  let ageOnMars = Math.floor(age / mars);
+  console.log("ageOnMars", ageOnMars);
+  let ageOnJupiter = Math.floor(age / jupiter);
+  console.log("ageOnJupiter", ageOnJupiter);
+}
