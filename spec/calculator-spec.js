03673267a8f26281faf09ad72358ba98127a7cf6
Marc Davies <marcdaviesriot@gmail.com>
2019-05-31 11:38:46 -0700

    add functioning spec directory including calculator-spec.js

---
+++ spec/calculator-spec.js
@@ -0,0 +1,10 @@
+import { Calculator } from './../src/calculator.js';
+
+describe('Calculator', function() {
+
+  // it('should replace 3 with ping', function() {
+  //   var arr = [1, 2, "ping"];
+  //   expect(pingPong(3)).toEqual(arr);
+  // });
+
+});
