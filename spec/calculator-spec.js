a140ccbb533dfc572f2ab794a67018627b044a9c
Marc Davies <marcdaviesriot@gmail.com>
2019-05-31 23:12:43 -0700

    pass tests for age calculation on all planets

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -1,10 +1,40 @@
-import { Calculator } from './../src/calculator.js';
+import { Calculator } from './../src/Calculator.js';
 
 describe('Calculator', function() {
 
-  it("should return the user's age on Earth", function() {
-    let age = 18;
-    expect(calculator.ageOnEarth).toEqual(18);
-  });
+    it('should calculate age on Earth', function ()
+    {
+        let birthdate = new Calculator(new Date(1983, 7, 11));
+        let ageOnEarth = birthdate.ageOnEarth();
+        expect(ageOnEarth).toEqual(35);
+    });
+
+    it('should calculate age on Mercury', function ()
+    {
+        let birthdate = new Calculator(new Date(1983, 11, 7));
+        let ageOnMercury = birthdate.ageOnMercury();
+        expect(ageOnMercury).toEqual(145);
+    });
+
+    it('should calculate age on Venus', function ()
+    {
+        let birthdate = new Calculator(new Date(1983, 11, 7));
+        let ageOnVenus = birthdate.ageOnVenus();
+        expect(ageOnVenus).toEqual(56);
+    });
+
+    it('should calculate age on Mars', function ()
+    {
+        let birthdate = new Calculator(new Date(1983, 11, 7));
+        let ageOnMars = birthdate.ageOnMars();
+        expect(ageOnMars).toEqual(18);
+    });
+
+    it('should calculate age on Jupiter', function ()
+    {
+        let birthdate = new Calculator(new Date(1983, 11, 7));
+        let ageOnJupiter = birthdate.ageOnJupiter();
+        expect(ageOnJupiter).toEqual(2);
+    });
 
 });
